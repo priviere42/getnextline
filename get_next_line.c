da035c807acd373782fea39ae1e0269d1bc384c9
Paul Riviere <priviere@z4r2p5.le-101.fr>
2019-11-25 11:03:04 +0100

    no leaks, new function

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: priviere <priviere@student.le-101.fr>      +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2019/10/30 12:43:11 by priviere     #+#   ##    ##    #+#       */
-/*   Updated: 2019/11/24 18:32:03 by priviere    ###    #+. /#+    ###.fr     */
+/*   Updated: 2019/11/25 11:02:31 by priviere    ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
@@ -52,29 +52,26 @@ int			ft_read_fd(int fd, char *buf, char **rest, char **line)
 	return (0);
 }
 
-// int ft_call_reading(int fd, char *buf, char *rest, char **line)
-// {
-// 	int ret;
-	
-// 	ret = ft_read_fd(fd, buf, &rest, line);
-// 	if (rest && (ret > 0 || *rest == '\0' || rest == NULL))
-// 	{
-// 		if (!ret && *line)
-// 		{
-// 			*line = ft_strdup("", &rest);
-// 			return (0);
-// 		}
-// 		return (1);
-// 	}
-// 	if (!rest)
-// 	{
-// 		*line = ft_strdup("", &rest);
-// 		return (0);
-// 	}
-// 	*line = rest;
-// 	rest = NULL;
-// 	return (0);
-// }
+int ft_call_reading(int ret, char **rest, char **line)
+{
+	if (*rest && (ret > 0 || **rest == '\0' || *rest == NULL))
+	{
+		if (!ret && *line)
+		{
+			*line = ft_strdup("", rest);
+			return (0);
+		}
+		return (1);
+	}
+	if (!*rest)
+	{
+		*line = ft_strdup("", rest);
+		return (0);
+	}
+	*line = *rest;
+	*rest = NULL;
+	return (0);
+}
 
 
 int			get_next_line(int fd, char **line)
@@ -82,7 +79,14 @@ int			get_next_line(int fd, char **line)
 	static char		*rest = NULL;
 	char			*buf;
 	int				ret;
+	int 			value;
 
+	buf = NULL;
+	if (fd < 0 || read(fd, buf, 0) < 0)
+	{
+		*line = ft_strdup("", &rest);
+		return (-1);
+	}
 	if (!rest)
 	{
 		rest = ft_strdup("", &rest);
@@ -94,26 +98,9 @@ int			get_next_line(int fd, char **line)
 			return (1);
     if (!(buf = (char *)malloc(sizeof(char) * BUFFER_SIZE + 1)))
         return (-1);
-	if (fd < 0 || read(fd, buf, 0) < 0)
-		return (-1);
 	ret = ft_read_fd(fd, buf, &rest, line);
-	if (rest && (ret > 0 || *rest == '\0' || rest == NULL))
-	{
-		if (!ret && *line)
-		{
-			*line = ft_strdup("", &rest);
-			return (0);
-		}
-		return (1);
-	}
-	if (!rest)
-	{
-		*line = ft_strdup("", &rest);
-		return (0);
-	}
-	*line = rest;
-	rest = NULL;
-	return (0);
+	value = ft_call_reading(ret, &rest, line);
+	return (value);
 }
 
 // #include <sys/stat.h>
